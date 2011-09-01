fac67166f4b91241afb43d47420204deb20a1f1e
sandywong <free_swallow8938@qq.com>
2011-09-01 12:44:11 +0800

    command line of http and socket edition

---
+++ httpEdition.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var appName = process.argv[2];
+var url = "";
+
+switch(process.argv[3]){
+  case "start":
+    url = "/app/" + appName + "/run";
+    break;
+  case "stop":
+    url = "/app/" + appName + "/stop";
+    break;
+  case "status":
+    url = "/app/" + appName;
+  default:
+    console.log("default");
+    break;
+  }
+
+
+console.log(url);
+
+var options = {
+  host: '127.0.0.1',
+  port: 1127,
+  path: url,
+  method: 'post'
+};
+
+var req = http.request(options, function(res){
+  console.log("got response:" + res.statusCode);
+  
+  console.log("headers:" + JSON.stringify(res.headers));
+  
+  res.setEncoding('utf8');
+  res.on('data', function(chunk){
+    console.log("body:" + chunk);
+  });
+  res.on('end', function() {
+    console.log('end');
+  });
+  res.on('error',function(err) {
+    console.log('res error', err);
+  });
+}).on('error', function(e){
+  console.log("got error:" + e.message);
+});
+
+req.end();
+
+
---
+++ socketEdition.js
@@ -0,0 +1,43 @@
+var net = require('net');
+
+var appName = process.argv[2];
+var action = process.argv[3];
+var command;
+switch(action){
+  case "start":
+  case "restart":
+  case "stop":
+  case "status":
+    command = action + " " + appName + "\n";
+    break;
+  case "stdout":
+  case "stderr":
+    command = action + " " + appName + " " + process.argv[4] + "\n";
+    break;
+  default:
+    console.log("error: undefined command! Please enter the right command!");
+}
+
+var socket = new net.Socket();
+
+socket.connect(1128);
+
+console.log("sending command...");
+socket.write(command);
+socket.on('data', function(data){
+  data = ""+data;
+  if(action != "stdout" && action != "stderr")
+    data = eval('('+data+')');
+  if(data.status == "error"){
+    console.log("error: " + data.msg);
+  }
+  else{
+    if(data.msg)
+      console.log(data.msg);
+    else
+      console.log(data);
+  }
+  //console.log(data.status);
+  socket.destroy();
+});
+
