fe2361376dc3d118278bb04cfa6b0c07e8182ecd
sandywong <free_swallow8938@qq.com>
2012-05-25 23:19:23 +0800

    Update httpEdition.js

--- httpEdition.js
+++ httpEdition.js
@@ -47,4 +47,9 @@ var req = http.request(options, function(res){
 
 req.end();
 
-
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World\n');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
